d7318d55196f3cb3dc39a8a7cf2f7533b2d0098e
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-05 14:31:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+
+function writeCards() { 
+  for (let i = 0, i < cards.length; i++) {
+    console.log("Thank you, ${cards[i], for the wondeful birthday gift!");
+  }
+  
+  return cards;
+}
+
+writeCards(cards);
